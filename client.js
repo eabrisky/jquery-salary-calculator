7292baf66e023137d4cb264c0fd5e1de91a9575d
Emry Brisky <eabrisky@gmail.com>
2021-05-16 13:05:32 -0500

    completed assignment without stretch goal

--- client.js
+++ client.js
@@ -1,5 +1,8 @@
 $(document).ready(readyNow);
 
+let empMon = [];
+// console.log(empMon);
+
 // F U N C T I O N S
 
 function readyNow(){
@@ -7,12 +10,12 @@ function readyNow(){
     // C L I C K   L I S T E N E R S
 
     $('#submitButton').on('click', handleForm)
-    $('#bodyBody').on('click', '.deleteButton', handleDelete);
+    $('#mainBody').on('click', '.deleteButton', handleDelete);
 } // end readyNow fn
 
 function handleForm(){
 
-    let employee = {
+    const employee = {
         firstName: $('#firstName').val(),
         lastName: $('#lastName').val(),
         employeeID: $('#idNumber').val(),
@@ -23,23 +26,24 @@ function handleForm(){
     
     // console.log(employee);
 
-    $('#bodyBody').last().append(`
+    calculateTotalMonthly();
+
+    $('#mainBody').last().append(`
         <tr class = "stuff">
             <td class = "employeeInformation">${employee.firstName}</td>
             <td class = "employeeInformation">${employee.lastName}</td>
             <td class = "employeeInformation">${employee.employeeID}</td>
             <td class = "employeeInformation">${employee.jobTitle}</td>
             <td class = "employeeInformation">${employee.annualSalary}</td>
-            <td class = "employeeInformation">${employee.monthlyCost}</td>
+            <td class = "employeeInformation">${employee.monthlyCost.toFixed(2)}</td>
             <td class = "employeeInformation">
                 <button class ="deleteButton">DELETE</button>
             </td>
         </tr>
-    `); // end bodyBody appendage
+    `); // end mainBody appendage
 
-    $('.input').val('');
+
     // clear input
-    calculateTotalMonthly();
 } // end handleForm fn
 
 function handleDelete(){
@@ -50,12 +54,38 @@ function handleDelete(){
 
 function calculateTotalMonthly(){
     // console.log('in calculateTotalMonthly');
-    let totalMonthly = [];
-    let employeeTotalCost = monthlyCost;
-    totalMonthly.push(employeeTotalCost);
-    console.log(monthlyCost);
-    // update the variable value 
-    $('#totalMonthlyCost').text(`Total Monthly Cost: ${totalMonthly}`);
+    // let totalMonthly = [];
+    // let employeeTotalCost = monthlyCost;
+    // totalMonthly.push(employeeTotalCost);
+    // console.log(monthlyCost);
+    // // update the variable value 
+    // $('#totalMonthlyCost').text(`Total Monthly Cost: ${totalMonthly}`);
+    
+    let anSal = $('#annualSalary').val();
+    // console.log(anSal);
+    let monCost = anSal / 12;
+    let totalMonthlyCost = 0;
+    // console.log(monCost);
+    empMon.push(monCost);
+    
+    for(let i = 0; i < empMon.length; i++){
+        totalMonthlyCost += parseInt(empMon[i]);
+        
+    } // end for loop
+
+     $('#totalMonthlyCost').text(`Total Monthly Cost: $${totalMonthlyCost.toFixed(2)}`)
+
+    // how to get the sum of every newly created monthly cost
+    // perhaps found in code block or bonus adjustment assignments?
+    
+    if(totalMonthlyCost >=20000){
+        $('#totalMonthlyCost').toggleClass('highlight')
+    } // end calculateMonthlyCost fn
+
+    // If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
+    // if statement >=20000, {
+        // add class of redBackground to $('#totalMonthlyCost')
+    // }
 } // end calculateTotalMonthly fn
 
 // create an empty array
